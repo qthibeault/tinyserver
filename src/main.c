6a2efb09656eda1637c799b22d8f0d0c7b60d051
Quinn Thibeault <quinn.thibeault96@gmail.com>
2016-05-28 14:33:09 -0700

    Port argument parsing

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-    puts("Hello World");
+    int port = 80;
+    
+    int i;
+    for(i = 1; i < argc - 1; ++i){
+        char *arg = argv[i];
+        if(arg[0] == '-'){
+            switch(arg[1]){
+                case 'p': {
+                    port = atoi(argv[++i]);
+                }
+            }
+        }
+    }
+    
+    printf("Bringing server online on port %d\n", port);
     return 0;
 }

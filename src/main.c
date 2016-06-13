10354c0702ba0079f26e30367f7e77bc98d0aebd
Quinn Thibeault <quinn.thibeault96@gmail.com>
2016-06-12 20:12:33 -0700

    Create socket descriptor

--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
-
+#include <stdlib.>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 #include "logging.h"
 
 int main(int argc, char* argv[])
 {
-    log_print(INFO, "Hello World");
+    int sock_desc;
+    struct sockaddr_in server;
+    
+    sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_desc < 0) {
+        log_print(ERROR, "Could not create socket");
+        return -1;
+    }
+    
     return 0;
 }
